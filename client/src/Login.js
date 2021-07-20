67c41508b4da9d1f0679ae750c571d48674e7498
jordan <jleejx2000@gmail.com>
2021-07-20 20:21:08 +0800

    login button with redirect to authorisation

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,8 +1,19 @@
 import React from "react";
 
+import { Container } from "react-bootstrap";
+
 const AUTH_URL =
   "https://accounts.spotify.com/authorize?client_id=08506ba73c4c463f92f75327d2c884a3&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 export default function Login() {
-  return <div></div>;
+  return (
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <a className="btn btn-success btn-lg" href={AUTH_URL}>
+        Login With Spotify
+      </a>{" "}
+    </Container>
+  );
 }
